1dc05bcbc2672998c2a5df4e3c8d4607343e1d96
unscript <alexkh34@gmail.com>
2017-04-23 16:13:29 +0300

    working version

---
+++ index.js
@@ -0,0 +1,43 @@
+var path = require("path");
+
+// private
+function stringToPath(val) {
+    val = val.trim().split(" ");
+    return path.join.apply(path, val);
+}
+
+// export
+function make(val) {
+    if (typeof val !== "string") {
+        throw new Error("value must be string");
+    }
+
+    return stringToPath(val);
+}
+
+// export
+function _Zpath(root) {
+    if (typeof root !== "string") {
+        throw new Error("value can be string");
+    }
+    this.root = root;
+}
+
+_Zpath.prototype.make = function (val) {
+    if (typeof val === "undefined" || val === null) {
+        return path.normalize(this.root);
+    }
+
+    console.log(typeof val.trim);
+
+    if (typeof val !== "string") {
+        throw new Error("value must be string, number, undefined or null");
+    }
+
+    return path.join(this.root, stringToPath(val));
+};
+
+exports.make = make;
+exports.create = function (val) {
+    return new _Zpath(val);
+}
